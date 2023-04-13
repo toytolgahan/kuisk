a89cfd1ece86b770556671859fbb2f1cc654f6e2
toytolgahan <tolgahantoyboun@gmail.com>
2023-04-13 19:31:56 +0300

    some debugging

--- discipline/views.py
+++ discipline/views.py
@@ -4,23 +4,33 @@ from .models import Discipline, Person
 # Create your views here.
 
 
-parent_discipline = Discipline.objects.get(title="Artificial Intelligence")
-main_disciplines = Discipline.objects.filter(parents=parent_discipline)
+def get_parent_discipline():
+    return Discipline.objects.get(title="Artificial Intelligence")
+
+def get_main_discipline():
+    parent_discipline = get_parent_discipline()
+    return Discipline.objects.filter(parents=parent_discipline)
+
 def home(request):
+    main_disciplines = get_main_discipline()
+    parent_discipline = get_parent_discipline()
     context =  {'main_disciplines':main_disciplines, 'parent_discipline':parent_discipline}
     return render(request, 'home.html', context)
 
 def why(request):
+    main_disciplines = get_main_discipline()
     context = {'main_disciplines':main_disciplines}
     return render(request,'why.html', context)
 
 def discipline_detail(request, discipline_slug):
+    main_disciplines = get_main_discipline()
     discipline = get_object_or_404(Discipline, slug=discipline_slug)
     subdisciplines = Discipline.objects.filter(parents=discipline)
     context = {'discipline': discipline, 'subdisciplines':subdisciplines,'main_disciplines':main_disciplines}
     return render(request, 'discipline_detail.html',context)
 
 def person_detail(request, pk):
+    main_disciplines = get_main_discipline()
     person = get_object_or_404(Person, pk=pk)
     context = {'person':person,'main_disciplines':main_disciplines}
     return render(request, 'person_detail.html',context)
