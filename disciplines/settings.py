d9cfc495b7344d18ed1cd93a05084e3dddaf3b9b
toytolgahan <tolgahantoyboun@gmail.com>
2023-04-13 18:57:51 +0300

    some changes

--- disciplines/settings.py
+++ disciplines/settings.py
@@ -77,12 +77,23 @@ WSGI_APPLICATION = "disciplines.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+# Use Heroku's DATABASE_URL when running on Heroku
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
 
 # Password validation
