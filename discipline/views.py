4d9fac57bca173884d22057efd825a0a31c759bc
toytolgahan <tolgahantoyboun@gmail.com>
2023-04-14 18:12:48 +0300

    some changes in views

--- discipline/views.py
+++ discipline/views.py
@@ -4,36 +4,25 @@ from .models import Discipline, Person
 # Create your views here.
 
 
-def get_parent_discipline():
-    print("we are here now ")
-    return get_object_or_404(Discipline, title="Artificial Intelligence")
+parent_discipline = Discipline.objects.filter(title="Artificial Intelligence")
+main_disciplines = Discipline.objects.filter(parent=parent_discipline)
 
-def get_main_discipline():
-    parent_discipline = get_parent_discipline()
-    print(parent_discipline)
-    return get_object_or_404(Discipline, parents=parent_discipline)
 
 def home(request):
-    print("homeee  ")
-    main_disciplines = get_main_discipline()
-    parent_discipline = get_parent_discipline()
     context =  {'main_disciplines':main_disciplines, 'parent_discipline':parent_discipline}
     return render(request, 'home.html', context)
 
 def why(request):
-    main_disciplines = get_main_discipline()
     context = {'main_disciplines':main_disciplines}
     return render(request,'why.html', context)
 
 def discipline_detail(request, discipline_slug):
-    main_disciplines = get_main_discipline()
     discipline = get_object_or_404(Discipline, slug=discipline_slug)
     subdisciplines = Discipline.objects.filter(parents=discipline)
     context = {'discipline': discipline, 'subdisciplines':subdisciplines,'main_disciplines':main_disciplines}
     return render(request, 'discipline_detail.html',context)
 
 def person_detail(request, pk):
-    main_disciplines = get_main_discipline()
     person = get_object_or_404(Person, pk=pk)
     context = {'person':person,'main_disciplines':main_disciplines}
     return render(request, 'person_detail.html',context)
