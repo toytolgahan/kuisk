ec31c309e460bfe631fc893263d7abe146ae47c7
toytolgahan <tolgahantoyboun@gmail.com>
2023-04-14 15:18:09 +0300

    database

--- disciplines/settings.py
+++ disciplines/settings.py
@@ -94,6 +94,15 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+"""
 
 # Update the database configuration with $DATABASE_URL.
 #db_from_env = dj_database_url.config(conn_max_age=500, env='HEROKU_POSTGRESQL_SILVER_URL')
