a85a7cbf464de7fb4c5ad099d187ff0a596976ea
Tolgahan Toy <tolgahantoyboun@gmail.com>
2023-04-14 21:17:44 +0300

    change in a template

--- discipline/models.py
+++ discipline/models.py
@@ -9,6 +9,7 @@ class Video(models.Model):
     title = models.CharField(max_length=255)
     link = models.URLField()
     body = models.TextField(blank=True)
+    
     def __str__(self):
         return self.title
 
