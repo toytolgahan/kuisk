226ddc80228d7ae59af57c0c2e4ff1fed080ee29
toytolgahan <tolgahantoyboun@gmail.com>
2023-04-17 19:11:47 +0300

    ai models added to the menu

--- discipline/views.py
+++ discipline/views.py
@@ -6,29 +6,33 @@ from .models import Discipline, Person, Work
 
 parent_discipline = Discipline.objects.get(title="Artificial Intelligence")
 main_disciplines = Discipline.objects.filter(parents__title="Artificial Intelligence")
-
+neural_models = [Discipline.object.get(title="Neural Networks")]
 
 
 def home(request):
     print("parent disciplines ",parent_discipline)
     print("main disciplines: ",main_disciplines)
     context =  {'main_disciplines':main_disciplines, 'parent_discipline':parent_discipline}
+    context['neural_models'] = neural_models
     return render(request, 'home.html', context)
 
 def why(request):
     context = {'main_disciplines':main_disciplines}
+    context['neural_models'] = neural_models
     return render(request,'why.html', context)
 
 def discipline_detail(request, discipline_slug):
     discipline = get_object_or_404(Discipline, slug=discipline_slug)
     subdisciplines = Discipline.objects.filter(parents=discipline)
     context = {'discipline': discipline, 'subdisciplines':subdisciplines,'main_disciplines':main_disciplines}
+    context['neural_models'] = neural_models
     return render(request, 'discipline_detail.html',context)
 
 def person_detail(request, pk):
     person = get_object_or_404(Person, pk=pk)
     works = Work.objects.filter(authors__id=person.id)
     context = {'person':person,'main_disciplines':main_disciplines,'works':works}
+    context['neural_models'] = neural_models
     return render(request, 'person_detail.html',context)
 
 
@@ -37,5 +41,6 @@ def search(request):
     discipline_results = Discipline.objects.filter(title__icontains=query)
     person_results = Person.objects.filter(last_name__icontains=query)
     context = {'discipline_results':discipline_results,'person_results':person_results, 'query':query, 'main_disciplines':main_disciplines}
+    context['neural_models'] = neural_models
     return render(request, 'search_results.html', context)
 
