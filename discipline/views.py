6fcc18404b8639d8c199e2f59e46e54c8dcb6375
toytolgahan <tolgahantoyboun@gmail.com>
2023-04-18 14:44:31 +0300

    some change in the menu and comments section added

--- discipline/views.py
+++ discipline/views.py
@@ -7,29 +7,37 @@ from .models import Discipline, Person, Work
 parent_discipline = Discipline.objects.get(title="Artificial Intelligence")
 main_disciplines = Discipline.objects.filter(parents__title="Artificial Intelligence")
 neural_models = Discipline.objects.filter(parents__title="Neural Networks")
+machine_learning = Discipline.objects.filter(parent__title="Machine Learning")
+reinforcement_learning = Discipline.objects.filter(parent__title="Reinforcement Learning")
+human_readable = Discipline.objects.filter(parent__title="Human-Readable Models")
+
+context = {}
+context['main_disciplines'] = main_disciplines
+context['neural_models'] = neural_models
+context['machine_learning'] = machine_learning
+context['reinforcement_learning'] = reinforcement_learning
+context['human_readable'] = human_readable
 
 
 def home(request):
-    context =  {'main_disciplines':main_disciplines, 'parent_discipline':parent_discipline}
-    context['neural_models'] = neural_models
+    context['parent_discipline'] = parent_discipline
     return render(request, 'home.html', context)
 
 def why(request):
-    context = {'main_disciplines':main_disciplines}
-    context['neural_models'] = neural_models
     return render(request,'why.html', context)
 
 def discipline_detail(request, discipline_slug):
     discipline = get_object_or_404(Discipline, slug=discipline_slug)
     subdisciplines = Discipline.objects.filter(parents=discipline)
-    context = {'discipline': discipline, 'subdisciplines':subdisciplines,'main_disciplines':main_disciplines}
-    context['neural_models'] = neural_models
+    context['discipline'] = discipline
+    context['subdisciplines'] = subdisciplines
     return render(request, 'discipline_detail.html',context)
 
 def person_detail(request, pk):
     person = get_object_or_404(Person, pk=pk)
     works = Work.objects.filter(authors__id=person.id)
-    context = {'person':person,'main_disciplines':main_disciplines,'works':works}
+    context['person']= person
+    context['works'] = works
     context['neural_models'] = neural_models
     return render(request, 'person_detail.html',context)
 
@@ -38,7 +46,8 @@ def search(request):
     query = request.GET.get('query', '')
     discipline_results = Discipline.objects.filter(title__icontains=query)
     person_results = Person.objects.filter(last_name__icontains=query)
-    context = {'discipline_results':discipline_results,'person_results':person_results, 'query':query, 'main_disciplines':main_disciplines}
-    context['neural_models'] = neural_models
+    context['discipline_results'] = discipline_results
+    context['person_results'] = person_results
+    context['query'] = query
     return render(request, 'search_results.html', context)
 
