bc561cecc4a72d9b34100d4aa041bdf54dc475f0
toytolgahan <tolgahantoyboun@gmail.com>
2023-04-18 15:42:00 +0300

    change in comment forms

--- comments/forms.py
+++ comments/forms.py
@@ -4,5 +4,5 @@ from .models import Comment
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['name','email','message']
+        fields = ['name','email','message', 'comment']
         
